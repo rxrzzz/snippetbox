d0fecccffb01aec12aa1fea951ed07cd4c5f2025
abdurrazaq <temiloluwagithub@gmail.com>
2023-11-28 12:08:55 +0100

    added handlers

---
+++ cmd/web/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/create", createSnippet)
+	mux.HandleFunc("/show", showSnippet)
+
+	fileServer := http.FileServer(http.Dir("../../ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	log.Fatal(err)
+}
