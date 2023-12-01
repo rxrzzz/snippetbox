02e05e9897e2e35f1edcb82faaf4c35d52a8bb23
abdurrazaq <temiloluwagithub@gmail.com>
2023-12-01 10:15:21 +0100

    added authentication methods

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -99,7 +99,24 @@ func (app *application) loginUserForm(w http.ResponseWriter, r *http.Request) {
 	})
 }
 func (app *application) loginUser(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Authenticate and login the user...")
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	form := forms.New(r.PostForm)
+	_, err = app.users.Authenticate(form.Get("email"), form.Get("password"))
+	if err == models.ErrInvalidCredentials {
+		form.Errors.Add("generic", "Email or password is incorrect")
+		app.render(w, r, "login.page.tmpl", &templateData{Form: form})
+		return
+	} else if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, "/create", http.StatusSeeOther)
+
 }
 func (app *application) logoutUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Logout the user...")
